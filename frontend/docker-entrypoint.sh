7fdeebe4a105ea3902b17a91a654d68bd10a4fab
shaseply <vspox23@gmail.com>
2026-09-25 18:51:46 +0300

    feat: fixed entrypoint

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -2,7 +2,12 @@
 set -eu
 
 : "${BACKEND_URL:=http://backend:4000}"
-sed "s|__BACKEND_URL__|${BACKEND_URL}|g" \
+
+BACKEND_HOST="${BACKEND_URL#http://}"
+BACKEND_HOST="${BACKEND_HOST#https://}"
+
+sed -e "s|__BACKEND_URL__|${BACKEND_URL}|g" \
+    -e "s|__BACKEND_HOST__|${BACKEND_HOST}|g" \
   /etc/nginx/templates/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
